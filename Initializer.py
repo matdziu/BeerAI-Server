46180bec6e21404b516e777c3238ca3bee712868
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-10-12 22:05:19 +0200

    Changed init process of inception-v3

--- Initializer.py
+++ Initializer.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
+from keras.applications import InceptionV3
 from keras.models import model_from_json
 
 PROJECT_PATH = "/Users/mateuszdziubek/Desktop/BeerAI-Server"
 
 
 def init():
-    bottleneck_features_extractor_file = open(f'{PROJECT_PATH}/models/bottleneck_features_extractor.json', 'r')
-    model_file = open(f'{PROJECT_PATH}/models/model.json', 'r')
+    model_file = open(f'{PROJECT_PATH}/model.json', 'r')
 
-    bottleneck_features_extractor = model_from_json(bottleneck_features_extractor_file.read())
+    bottleneck_features_extractor = InceptionV3(include_top=False, weights='imagenet', input_shape=(150, 150, 3))
     model = model_from_json(model_file.read())
     model.load_weights('beer_label_classifier_weights.h5')
 
