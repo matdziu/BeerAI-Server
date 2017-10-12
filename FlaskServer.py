5c2e821fb2155b7c251da9ede07259c50010f942
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-10-12 13:24:07 +0200

    Created Flask server for predictions ('/predict')

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,8 +1,36 @@
-# import numpy as np
-# from keras.preprocessing.image import load_img, img_to_array
-#
-# image_path = "test.jpg"
-# image = load_img(image_path, target_size=(150, 150))
-# image = img_to_array(image)
-# image = np.expand_dims(image, axis=0)
-# image = image / 255
+import os
+
+import numpy as np
+from flask import Flask, request
+from keras.preprocessing.image import load_img, img_to_array
+
+from Classifier import classify
+from Initializer import init
+
+app = Flask(__name__)
+
+bottleneck_features_extractor, model, graph = init()
+
+to_predict_path = "/Users/mateuszdziubek/Desktop/BeerAI-Server"
+to_predict_name = "image_to_predict.jpg"
+
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    image_file = request.files['image_file']
+    image_file.save(os.path.join(to_predict_path, to_predict_name))
+    image = load_img(os.path.join(to_predict_path, to_predict_name), target_size=(150, 150))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = image / 255
+
+    with graph.as_default():
+        bottleneck_feature = bottleneck_features_extractor.predict(image)
+        prediction_encoded = model.predict(bottleneck_feature)
+        response = classify(prediction_encoded)
+        return response
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- Initializer.py
+++ Initializer.py
@@ -1,16 +1,25 @@
+import numpy as np
 import tensorflow as tf
 from keras.applications import InceptionV3
 from keras.layers import Flatten, Dense, Dropout
 from keras.models import Sequential
+from keras.preprocessing.image import load_img, img_to_array
 
 from Classifier import output_beer_labels
+
+image_path = "test.jpg"
+image = load_img(image_path, target_size=(150, 150))
+image = img_to_array(image)
+image = np.expand_dims(image, axis=0)
+image = image / 255
 
 
 def init():
     bottleneck_features_extractor = InceptionV3(include_top=False, weights='imagenet', input_shape=(150, 150, 3))
+    test_bottleneck_feature = bottleneck_features_extractor.predict(image)
 
     model = Sequential()
-    model.add(Flatten(input_shape=(2048, 1)))
+    model.add(Flatten(input_shape=test_bottleneck_feature.shape[1:]))
     model.add(Dense(512, activation='relu'))
     model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.5))
